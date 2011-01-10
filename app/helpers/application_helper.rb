7b37c281967950049f2390fb7e1aa7c23860ce60
Dou <dou@buzzlabs.com>
2011-01-10 10:14:47 -0800

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo
+    image_tag("doushen.jpg", :alt => "Sample App", :class => "round")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  
+  
   get "users/new"
+  get "users/show"
   
   match '/signup',  :to => 'users#new'
 
